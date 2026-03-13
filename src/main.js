a78ef76bbe948e2135ceb908419ac1e4b0267def
Yudelka Reyes <yudelkateran@gmail.com>
2026-03-12 21:55:39 -0300

    Agregando

--- src/main.js
+++ src/main.js
@@ -3,20 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// 1. Primero los estilos base de librerías
+// 1. Estilos de librerías
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import 'bootstrap'
-import './assets/main.scss'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
- 
-.instagram-hover:hover {
-    background-color: #E1306C !important;
-    border-color: #E1306C !important;
-}
 
-.whatsapp-hover:hover {
-    background-color: #25D366 !important;
-    border-color: #25D366 !important;
-}
+// 2. Tus estilos personalizados (SASS)
+import './assets/main.scss'
+
+// 3. JavaScript de Bootstrap (La versión bundle ya incluye Popper.js)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 createApp(App).use(store).use(router).mount('#app')
